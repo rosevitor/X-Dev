b606d665fb2ffc880bd0a573a4ceffff8e19de6f
rosevitor <rosenbaum.maia.vitor@hotmail.com>
2024-06-09 21:15:08 -0300

    JS incluido

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,50 @@
 /* 
-- Objetivo 1 - Quando o usuário clicar no botão de ir pro próximo
-  Passo 1 - pegar o botão mostrar mais no JS pra poder verificar quando o usuário clicar em cima dele 
-  Passo 2 - identificar o clique no botão 
-  Passo 3 - adicionar a classe ativo nos projetos escondidos 
+OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o proximo 
+cartao da lista
+  
+passo 1 - dar um jeito de pegar o elemento HTML da seta avancar
+passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+passo 3 - fazer aparecer o próximo cartão da lista
+passo 4 - buscar o cartão que esta selecionado e esconder
 
-- Objetivo 2 - esconder o botão de mostrar mais 
-  Passo 1 - pegar o botão e esconder ele
-  */
+OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão 
+anterior da lista
+  
+passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+passo 2 - dar um jeito de identificar o clique do usuário na seta voltar
+passo 3 - fazer aparecer o cartão anterior da lista
+passo 4 - buscar o cartão que esta selecionado e esconder */
 
-const botaoVoltar = document.querySelector('btn-voltar');
-const botaoAvancar = document.querySelector('btn-avancar');
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
 
-botaoAvancar.addEventListener('click', () => {
-    avancarCartao();
+btnAvancar.addEventListener("click", function () {
+  const ehUltimoCartao = cartaoAtual === cartoes.length - 1;
+  if (ehUltimoCartao) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual++;
+  mostrarCartao();
 });
 
-botaoVoltar.addEventListener('click', () => {
-    voltarCartao();
+btnVoltar.addEventListener("click", function () {
+  const ehPrimeiroCartao = cartaoAtual === 0;
+  if (ehPrimeiroCartao) return;
+
+  esconderCartaoSelecionado();
+
+  cartaoAtual--;
+  mostrarCartao();
 });
 
-function avancarCartao() {
-    botaoMostrarProjetos.classList.add('remover');
+function mostrarCartao() {
+  cartoes[cartaoAtual].classList.add("selecionado");
 }
 
-function voltarCartao() {
-    const cartaoAtual = document.querySelector('.cartao.selecionado');
-
-    cartaoAtual.closest('li').nextElementSibling.classList.add('selecionado')
+function esconderCartaoSelecionado() {
+  const cartaoSelecionado = document.querySelector(".selecionado");
+  cartaoSelecionado.classList.remove("selecionado");
 }
